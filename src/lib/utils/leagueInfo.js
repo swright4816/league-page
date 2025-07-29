96da425dd4b88ee2020ce1e5c6d685c03355e4d1
swright4816 <swright4816@gmail.com>
2025-07-29 13:58:04 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -87,7 +87,7 @@ export const managers = [
          "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
          "rival": {
            name: "Tyler", // Can be anything (usually your rival's name)
-           link: 4, // manager array number within this array, or null to link back to all managers page
+    //       link: 4, // manager array number within this array, or null to link back to all managers page
            image: "/managers/tylergoofy.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
          },
     //   "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
@@ -99,7 +99,7 @@ export const managers = [
     },
   
     {
-         "roster": 4,  // (DEPRECATED! Don't use this anymore) ID of the roster that the manager manages (look at the order of the power rankings graph)
+    //     "roster": 4,  // (DEPRECATED! Don't use this anymore) ID of the roster that the manager manages (look at the order of the power rankings graph)
          "managerID": "986463156975271936",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
          "name": "Tyler",
     //   "tookOver": 2020, // (DEPRECATED! You don't need to use this anymore) (optional) used if a manager took over a team, delete this line or change to null otherwise
