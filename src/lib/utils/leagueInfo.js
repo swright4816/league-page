96792135ad4f74a70c65d722f77a433a650a125c
swright4816 <swright4816@gmail.com>
2025-07-29 09:08:27 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -28,6 +28,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 export const managers = [
      {
     //  "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+       "managerID": "741038570088894464",
        "name": "SammySlayz",
     //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
     //   "location": "Brooklyn", // (optional)
