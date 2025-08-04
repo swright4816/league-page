19f15f9e2fabffa7c2c163ebc1d7b6b0f373fd9f
swright4816 <swright4816@gmail.com>
2025-08-03 21:16:39 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
  /*   STEP 1   */
-export const leagueID = "1117890465669517312"; // your league ID
+export const leagueID = "1220061871822868480"; // your league ID
 export const leagueName = "🏆 Welcome to the Champion’s Club"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
